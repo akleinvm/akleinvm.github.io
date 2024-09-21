f93b9f7d3d750bbef840712ee544645a7e1e7ffa
akleinvm <akleinvm@gmail.com>
2024-09-21 11:16:00 +0800

    Replace with dist files

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: '/akleinvm.github.io/',
-  plugins: [react()],
-})
